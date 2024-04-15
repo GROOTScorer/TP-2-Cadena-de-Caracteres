caefb13c74adac40fddfd8f0975f128a3855f2a9
GROOTScorer <gaeldlk@gmail.com>
2024-04-15 14:01:27 -0300

    Agrega 3 puntos de la 8

---
+++ Ejercicio8b.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int i=0, len=0;
+
+char oracion[50];
+
+main()
+{
+    printf("Ingrese una oracion: ");
+    fgets(50, oracion, stdin);
+
+    len=strlen(oracion);
+
+    for(i=0;i<len;i++)
+    {
+        if(oracion[i] >= 65 && oracion[i] <= 90 && oracion[i] != ' ')
+        {
+            oracion[i]=oracion[i]^32;
+        }
+    }
+
+    printf("La palabra en todo minusculas es %s", oracion);
+}
